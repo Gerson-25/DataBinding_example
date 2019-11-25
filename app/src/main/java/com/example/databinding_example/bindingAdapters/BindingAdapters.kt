06261b74e0fa9f0a50a0c448eebb897b0aa2a773
Gerson Beltran <gmisaelbeltran@gmail.com>
2019-11-25 15:33:58 -0600

    modifying bindingAdapter

--- app/src/main/java/com/example/databinding_example/bindingAdapters/BindingAdapters.kt
+++ app/src/main/java/com/example/databinding_example/bindingAdapters/BindingAdapters.kt
@@ -4,7 +4,7 @@ import android.view.View
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("app:visibility")
+@BindingAdapter("app:hideIfZero")
     fun hideIfZero(view:ProgressBar, count:Int){
 view.visibility = if ( count == 0 ) View.GONE else View.VISIBLE
 }
